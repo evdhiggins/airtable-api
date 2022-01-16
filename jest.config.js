bee5d311c85abe13c936379de761381f6299d7d8
Evan Higgins <evdhiggins@gmail.com>
2022-01-16 15:33:19 -0600

    Move jest config into separate file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+    },
+    setupFiles: ['dotenv/config'],
+    testPathIgnorePatterns: ['/node_modules/', '/built/'],
+};
